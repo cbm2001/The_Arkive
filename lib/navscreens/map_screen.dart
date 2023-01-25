e3affc6647bd0f35e73b50b909cbe2bbf1d1d7d6
fp17 <fp17@hw.ac.uk>
2023-01-25 20:52:14 +0400

    map search completed

--- lib/navscreens/map_screen.dart
+++ lib/navscreens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:first_app/reusable_widgets/position_services.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../models/side_nav_bar.dart';
@@ -15,17 +16,12 @@ class MapPage extends StatefulWidget {
 class MapSampleState extends State<MapPage> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
+  TextEditingController _originController = TextEditingController();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   Marker _marker = Marker(
     markerId: MarkerId('marker 1'),
@@ -39,24 +35,58 @@ class MapSampleState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        markers: {_marker},
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      appBar: AppBar(
+        title: Text('Google Maps'),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+      body: Column(
+        children: [
+          Container(
+            child: Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: TextFormField(
+                      controller: _originController,
+                      textCapitalization: TextCapitalization.words,
+                      decoration: InputDecoration(hintText: 'Search Place...'),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        final place = await PositionServices()
+                            .getPlaceDetails(_originController.text);
+
+                        //update camera position
+                        _goToPlace(place['geometry']['location']['lat'],
+                            place['geometry']['location']['lng']);
+                      },
+                      child: Text('Search Place...'))
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: GoogleMap(
+              markers: {_marker},
+              mapType: MapType.normal,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToPlace(double lat, double lng) async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(lat, lng), zoom: 12)));
   }
 }
