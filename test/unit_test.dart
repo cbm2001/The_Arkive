4b881d4bb9f9d6ae0b6ecbe4253a98a9e1fa2edf
NripeshN <nn2012@hw.ac.uk>
2023-01-30 01:25:01 +0400

    Unit tests(uploadDraft, uploadPost) --failing

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,52 +1,55 @@
-/*import 'package:firebase_auth/firebase_auth.dart';
-//import 'package:flutter_auth/model/user_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/resources/auth_methods.dart';
-import '../lib/resources/firestore_methods.dart';
-import 'package:mockito/mockito.dart';
-import 'package:rxdart/rxdart.dart';
-
-class MockFirebaseAuth extends Mock implements FirebaseAuth {}
-
-class MockFirebaseUser extends Mock implements FirebaseUser {}
-
-class MockAuthResult extends Mock implements AuthResult {}
+import 'package:first_app/resources/firestore_methods.dart';
+import 'dart:io';
 
 void main() {
-  MockFirebaseAuth _auth = MockFirebaseAuth();
-
-  BehaviorSubject<MockFirebaseUser> _user = BehaviorSubject<MockFirebaseUser>();
-  when(_auth.onAuthStateChanged).thenAnswer((_) {
-    return _user;
+  test('example test', () {
+    expect(1+1, 2);
   });
-  //UserRepository _repo = UserRepository.instance(auth: _auth);
-  AuthMethods _repo = AuthMethods();
-  group('user repository test', () {
-    when(_auth.signInWithEmailAndPassword(email: "email", password: "password"))
-        .thenAnswer((_) async {
-      _user.add(MockFirebaseUser());
-      return MockAuthResult();
-    });
-    when(_auth.signInWithEmailAndPassword(email: "mail", password: "pass"))
-        .thenThrow(() {
-      return null;
-    });
-    test("sign in with email and password", () async {
-      bool signedIn =
-          await _repo.loginUser(email: "email", password: "password");
-      expect(signedIn, true);
-      // expect(_repo.status, Status.Authenticated);
+
+  group('FireStoreMethods test', () {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    FireStoreMethods fireStoreMethods;
+    FirebaseApp app;
+    setUpAll(() async {
+      app = await Firebase.initializeApp();
+      fireStoreMethods = FireStoreMethods();
     });
 
-    test("sing in fails with incorrect email and password", () async {
-      bool signedIn = await _repo.loginUser(email: "mail", password: "pass");
-      expect(signedIn, false);
-      // expect(_repo.status, Status.Unauthenticated);
+    test('uploadPost', () async {
+      String description = 'Test description';
+      File file = File('/test/8a1bdf8266f9a2d01d583c4c6dc17765.png');
+      Uint8List fileBytes = await file.readAsBytesSync();
+      String uid = '6VMj7t30QTfifDTOYfszxZUO7p73';
+      String username = 'poi';
+      String location = 'Test location';
+      String category = 'lifestyle';
+      String profImage =
+          'https://firebasestorage.googleapis.com/v0/b/my-app-73197.appspot.com/o/profilePics%2FynEiyELXQAclMETFPPWBdd4e3Xo1?alt=media&token=b670c039-3637-4977-9834-195bb5e1f0a6';
+      GeoPoint geoLoc = GeoPoint(0, 0);
+      String result = await fireStoreMethods.uploadPost(description, fileBytes,
+          uid, username, location, category, profImage, geoLoc);
+      expect(result, 'success');
     });
 
-    test('sign out', () async {
-      await _repo.signOut();
-      expect(_repo.status, Status.Unauthenticated);
+    test('uploadDraft', () async {
+      String description = 'Test description';
+      File file = File('/test/8a1bdf8266f9a2d01d583c4c6dc17765.png');
+      Uint8List fileBytes = await file.readAsBytesSync();
+      String uid = '6VMj7t30QTfifDTOYfszxZUO7p73';
+      String username = 'poi';
+      String location = 'Test location';
+      String category = 'lifestyle';
+      String profImage =
+          'https://firebasestorage.googleapis.com/v0/b/my-app-73197.appspot.com/o/profilePics%2FynEiyELXQAclMETFPPWBdd4e3Xo1?alt=media&token=b670c039-3637-4977-9834-195bb5e1f0a6';
+      GeoPoint geoLoc = GeoPoint(0, 0);
+      String result = await fireStoreMethods.uploadDraft(description, fileBytes,
+          uid, username, location, category, profImage, geoLoc);
+      expect(result, 'success');
     });
   });
-}*/
+}
