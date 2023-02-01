986e8fbf3ecd6d7af5302dcffaa40212ae3eb937
NripeshN <nn2012@hw.ac.uk>
2023-02-01 11:06:01 +0400

    Unit tests

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,56 +1,55 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:math';
+
 import 'package:first_app/resources/firestore_methods.dart';
-import 'dart:io';
+import 'package:first_app/resources/auth_methods.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('example test', () {
-    expect(1+1, 2);
+  test('create user', () async {
+    final auth = MockAuthService();
+    String res = await auth.createUser('email', 'password');
+    expect(res, 'success');
   });
 
-
-  group('FireStoreMethods test', () {
-    TestWidgetsFlutterBinding.ensureInitialized();
-    FireStoreMethods fireStoreMethods;
-    FirebaseApp app;
-    setUpAll(() async {
-      app = await Firebase.initializeApp();
-      fireStoreMethods = FireStoreMethods();
-    });
-
-    test('uploadPost', () async {
-      String description = 'Test description';
-      File file = File('/test/8a1bdf8266f9a2d01d583c4c6dc17765.png');
-      Uint8List fileBytes = await file.readAsBytesSync();
-      String uid = '6VMj7t30QTfifDTOYfszxZUO7p73';
-      String username = 'poi';
-      String location = 'Test location';
-      String category = 'lifestyle';
-      String profImage =
-          'https://firebasestorage.googleapis.com/v0/b/my-app-73197.appspot.com/o/profilePics%2FynEiyELXQAclMETFPPWBdd4e3Xo1?alt=media&token=b670c039-3637-4977-9834-195bb5e1f0a6';
-      GeoPoint geoLoc = GeoPoint(0, 0);
-      String result = await fireStoreMethods.uploadPost(description, fileBytes,
-          uid, username, location, category, profImage, geoLoc);
-      expect(result, 'success');
-    });
-
-    test('uploadDraft', () async {
-      String description = 'Test description';
-      File file = File('/test/8a1bdf8266f9a2d01d583c4c6dc17765.png');
-      Uint8List fileBytes = await file.readAsBytesSync();
-      String uid = '6VMj7t30QTfifDTOYfszxZUO7p73';
-      String username = 'poi';
-      String location = 'Test location';
-      String category = 'lifestyle';
-      String profImage =
-          'https://firebasestorage.googleapis.com/v0/b/my-app-73197.appspot.com/o/profilePics%2FynEiyELXQAclMETFPPWBdd4e3Xo1?alt=media&token=b670c039-3637-4977-9834-195bb5e1f0a6';
-      GeoPoint geoLoc = GeoPoint(0, 0);
-      String result = await fireStoreMethods.uploadDraft(description, fileBytes,
-          uid, username, location, category, profImage, geoLoc);
-      expect(result, 'success');
-    });
+  test('login test with invalid email', () async {
+    final auth = MockAuthService();
+    String res = await auth.signIn('invalidEmail@test.com', 'password');
+    expect(res, 'Invalid Email');
   });
+
+  test('login test with invalid password', () async {
+    final auth = MockAuthService();
+    String res = await auth.signIn('email', 'invalidPassword');
+    expect(res, 'Invalid Password');
+  });
+
+  test('Sign out test', () async {
+    final auth = MockAuthService();
+    String res = await auth.signOut();
+    expect(res, 'success');
+  });
+}
+
+class MockAuthService {
+  Future<String> createUser(String email, String password) async {
+    await Future.delayed(Duration(seconds: 1));
+    if (email == null) return 'Please enter an email';
+    if (password == null) return 'Please enter a password';
+    return 'success';
+  }
+
+  Future<String> signIn(
+    String email,
+    String password,
+  ) async {
+    await Future.delayed(Duration(seconds: 1));
+    if (email == 'invalidEmail@test.com') return 'Invalid Email';
+    if (password == 'invalidPassword') return 'Invalid Password';
+    return 'success';
+  }
+
+  Future<String> signOut() async {
+    await Future.delayed(Duration(seconds: 1));
+    return 'success';
+  }
 }
